905f3a68700a38c317d35f3ed0c7f46880059719
emmanuel-abraham <119742811+emmanuel-abraham@users.noreply.github.com>
2023-01-30 18:47:09 +0530

    30 Questions JavaScript

---
+++ 8th-Jan JavaScript 30 Questions/Question 01/variable.js
@@ -0,0 +1,14 @@
+//Q.1 Create a variable.js file and declare variables and assign string, boolean, undefined and null data types, Display all the value with their data type.
+
+// Declaring Variables
+
+let Name = "Emmanuel";
+let isMale = true;
+let havePet = null;
+let isMarried
+
+
+console.log("The data type of " + Name + " is " + typeof Name)
+console.log("The data type of " + isMale + " is " + typeof isMale)
+console.log("The data type of " + havePet + " is " + typeof havePet)
+console.log("The data type of " + isMarried + " is " + typeof isMarried)
