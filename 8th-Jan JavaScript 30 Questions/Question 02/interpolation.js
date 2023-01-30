228b351fdde8c0677d2c81c842d4bf0a82932082
emmanuel-abraham <119742811+emmanuel-abraham@users.noreply.github.com>
2023-01-31 02:41:51 +0530

    30 Question JavaScript

---
+++ 8th-Jan JavaScript 30 Questions/Question 02/interpolation.js
@@ -0,0 +1,13 @@
+//02. Declare variables to store your first name, last name, marital status, country and age and display them using interploation method.
+
+
+
+const firstName = "Emmanuel"
+const lastName = "Abraham"
+let maritalStatus = "Unmarried"
+let country = "India";
+let age = "23";
+
+const intro = `Hello! My name is ${firstName} ${lastName} and I am ${age} years old. I live in ${country} and currently ${maritalStatus}`;
+
+console.log(intro);
