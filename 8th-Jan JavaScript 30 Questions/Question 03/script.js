365662edc3c64358c85d2e0d8a18a2e7daf14740
emmanuel-abraham <119742811+emmanuel-abraham@users.noreply.github.com>
2023-02-10 14:57:29 +0530

    JS 30 Qestions

---
+++ 8th-Jan JavaScript 30 Questions/Question 03/script.js
@@ -0,0 +1,8 @@
+//03. Declare a varibale and assign string value to it and change all the string characters to capital letters using toUpperCase() method.
+
+
+
+let str = "This is a javacript assignment for FSJS 2.0"
+const strAllCapital = str.toUpperCase();
+
+console.log(strAllCapital);
