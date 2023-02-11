6bb72ad31862e24070f6a8695f1b3caaa9604d5b
emmanuel-abraham <119742811+emmanuel-abraham@users.noreply.github.com>
2023-02-11 14:57:36 +0530

    JS 30 Questions

---
+++ 8th-Jan JavaScript 30 Questions/Question 04/script.js
@@ -0,0 +1,7 @@
+//04. Declare a varibale and assign string value to it and check if the string contains a word Script using includes() method
+
+const str = "While Adding a Javscript file in HTML, we use the Script tag ";
+const isScriptIncludes = str.includes("Script");
+
+//output
+console.log(isScriptIncludes);
