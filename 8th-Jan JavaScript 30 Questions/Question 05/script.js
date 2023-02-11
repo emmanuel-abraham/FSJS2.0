2c656224244b00002e5991e38551820fdb11bd8e
emmanuel-abraham <119742811+emmanuel-abraham@users.noreply.github.com>
2023-02-12 03:41:17 +0530

    JS 30Questions

---
+++ 8th-Jan JavaScript 30 Questions/Question 05/script.js
@@ -0,0 +1,7 @@
+//05. Declare a varibale and assign string value to it and then split it into an array using split() method
+
+const str = "JavaScript is one of the most popular languages. It ranked number one on stackoverflow"
+const arr = str.split(" ");  //splitting the string based on whitespace
+
+//output
+console.log(arr);
